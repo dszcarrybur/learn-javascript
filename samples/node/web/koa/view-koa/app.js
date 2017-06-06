0cc015cb83d94f1dc7f7bccd1a3f1d630017e67f
carrybur <ding_shaoze@163.com>
2017-06-06 22:28:12 +0800

    Update app.js
    RPD0606，mvc框架下至少需要哪些模块来进行界面显示操作

--- samples/node/web/koa/view-koa/app.js
+++ samples/node/web/koa/view-koa/app.js
@@ -9,9 +9,8 @@ const templating = require('./templating');
 const app = new Koa();
 
 const isProduction = process.env.NODE_ENV === 'production';
-
 //获取开始时间截止时间及url
-// log request URL:
+// log request URL:打印开始时间
 app.use(async (ctx, next) => {
     console.log(`Process ${ctx.request.method} ${ctx.request.url}...`);
     var
@@ -22,23 +21,24 @@ app.use(async (ctx, next) => {
     ctx.response.set('X-Response-Time', `${execTime}ms`);
 });
 
-// static file support:
+// static file support:将bootstrap的static文件导入
 if (! isProduction) {
     let staticFiles = require('./static-files');
     app.use(staticFiles('/static/', __dirname + '/static'));
 }
 
-// parse request body:
+// parse request body:解析post请求中的参数
 app.use(bodyParser());
 
-// add nunjucks as view:
+// add nunjucks as view:导入nunjunk，及添加render（view，model）函数
 app.use(templating('views', {
     noCache: !isProduction,
     watch: !isProduction
 }));
 
-// add controller:
+// add controller:添加controllers--middleware
 app.use(controller());
 
 app.listen(3000);
 console.log('app started at port 3000...');
+
