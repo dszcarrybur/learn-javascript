52e13baa65122f6b22845436fcf9f7935f7b413b
dszcarrybur <ding_shaoze@163.com>
2017-06-09 00:21:43 +0800

    RPD0608,查看REACT及api使用方法

--- samples/node/web/rest/rest-hello/controllers/api.js
+++ samples/node/web/rest/rest-hello/controllers/api.js
@@ -7,8 +7,8 @@ var products = [{
 }];
 
 module.exports = {
-    'GET /api/products': async (ctx, next) => {
-        ctx.response.type = 'application/json';
+    'GET /api/products': async (ctx, next) => {///为将API和其他url地址请求分开前面api，如'/api/products'
+        ctx.response.type = 'application/json';//请求类型和返回数据类型都必须为'application/json'，且一般返回类型为json
         ctx.response.body = {
             products: products
         };
