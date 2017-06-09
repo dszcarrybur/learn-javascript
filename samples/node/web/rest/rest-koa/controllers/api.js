9fcf0d982c123616576be348fd6d03321777fd42
carrybur <ding_shaoze@163.com>
2017-06-09 17:26:09 +0800

    Update api.js
    RPD0609，rest-api调用方法

--- samples/node/web/rest/rest-koa/controllers/api.js
+++ samples/node/web/rest/rest-koa/controllers/api.js
@@ -1,10 +1,10 @@
-const products = require('../products');
+const products = require('../products');///此处作为service服务存在
 
 const APIError = require('../rest').APIError;
 
 module.exports = {
     'GET /api/products': async (ctx, next) => {
-        ctx.rest({
+        ctx.rest({///rest为反馈数据的方法，其输入参数只为要给客户端的数据
             products: products.getProducts()
         });
     },
@@ -14,13 +14,13 @@ module.exports = {
         ctx.rest(p);
     },
 
-    'DELETE /api/products/:id': async (ctx, next) => {
+    'DELETE /api/products/:id': async (ctx, next) => {///其中/api/products/:id的id变量值，获取形式为“:id”，获取通过ctx.params.id，一个url中可以有多个类似“:id”的存在
         console.log(`delete product ${ctx.params.id}...`);
         var p = products.deleteProduct(ctx.params.id);
         if (p) {
             ctx.rest(p);
         } else {
-            throw new APIError('product:not_found', 'product not found by id.');
+            throw new APIError('product:not_found', 'product not found by id.');///错误时主动报错错误及提供错误类型和代码
         }
     }
 };
